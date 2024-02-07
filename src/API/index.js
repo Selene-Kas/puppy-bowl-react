2376e3fe8344cf647ac61c36e8a471203d3d3c90
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-06 22:25:29 -0800

    import and export to all pages

---
+++ src/API/index.js
@@ -0,0 +1,11 @@
+import AllPlayers from "../components/AllPlayers";
+import NavBar from "../components/NavBar";
+import NewPlayerForm from "../components/NewPlayerForm";
+import SinglePlayer from "../components/SinglePlayer";
+
+
+const Index = () => {
+
+}
+
+export { AllPlayers, NavBar, NewPlayerForm, SinglePlayer, Index};
