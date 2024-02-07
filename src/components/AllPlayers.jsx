f7bcfc5f8158f2dfad3958bf0e52df694e82b42f
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-06 22:28:22 -0800

    added useState and use Effect

---
+++ src/components/AllPlayers.jsx
@@ -0,0 +1,18 @@
+import { Index } from "../API";
+import { useState, useEffect } from "react";
+
+const AllPlayers = () => {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(()=>{
+
+  },[])
+  
+  return (
+    <div>
+      <h1>All Players</h1>
+    </div>
+  );
+}
+
+export default AllPlayers;
