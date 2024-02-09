12f28c21f0ec3a809d8f2981bd5b75296eaa3b38
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-08 22:05:17 -0800

    deleted useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
-import { AllPlayers, NavBar, NewPlayerForm, SinglePlayer } from './API'
-import { useState } from 'react'
+import { AllPlayers, NavBar, NewPlayerForm, SinglePlayer } from './components'
 import './App.css'
 
-function App() {
-  //const [count, setCount] = useState(0)
+function App() { 
 
   return (
     <>
       <div>
         <NavBar />
         <main>
-          <h1>Puppy Bowl Site</h1>
+          <h1>Puppy Bowl</h1>
             <Routes>
               <Route path="/" element={<AllPlayers />}/>
               <Route path="singlePlayer" element={<SinglePlayer />} />
