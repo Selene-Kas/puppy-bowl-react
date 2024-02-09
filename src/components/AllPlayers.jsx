a71e5e71b94f31b10c3da74874cde628be0f8a50
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-08 22:09:55 -0800

    render all players and navigat to single player component

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,16 +1,35 @@
-import { Index } from "../API";
+import { fetchAllPlayers } from "../API";
 import { useState, useEffect } from "react";
+import SinglePlayer from "./SinglePlayer";
+import { useNavigate } from "react-router-dom";
 
 const AllPlayers = () => {
   const [players, setPlayers] = useState([]);
 
   useEffect(()=>{
+    async function getPlayersList() {
+        const playersList = await fetchAllPlayers()
+        setPlayers(playersList)
+    }
+    getPlayersList()
+  }, [])
+  //fetchAllPlayers();
 
-  },[])
-  
+  const navigate = useNavigate()
+
   return (
     <div>
       <h1>All Players</h1>
+      { players.map(player => {
+        return (
+            <div key={player.name}>
+                <h2>{player.name}</h2>
+                <img className="puppyImg" src={player.imageUrl} />
+                <button onClick={ () => navigate('/SinglePlayer')}> Player Details </button>
+            </div>
+        )
+      })
+      }
     </div>
   );
 }
