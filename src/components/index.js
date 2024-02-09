730ae7e6e52bbd18587a04be87342951d6c103c9
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-08 22:07:54 -0800

    imports for components

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import AllPlayers from "./AllPlayers";
+import NavBar from "./NavBar";
+import NewPlayerForm from "./NewPlayerForm";
+import SinglePlayer from "./SinglePlayer";
+
+
+
+export { AllPlayers, NavBar, NewPlayerForm, SinglePlayer};
