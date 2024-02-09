908466d9b872318060d7e1d02992a40f73af0034
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-08 22:10:56 -0800

    added useState hook

--- src/components/SinglePlayer.jsx
+++ src/components/SinglePlayer.jsx
@@ -1,7 +1,26 @@
+//import { fetchSinglePlayer } from '../API'
+import { useState, useEffect} from 'react';
+//import { Routes, Route, useParams } from 'react-router-dom'
+
 const SinglePlayer = () => {
+    const [player, setPlayer] = useState([]);
+
+    //useEffect(()=>{
+    //    async function getPlayer() {
+    //        const playerDetails = await fetchSinglePlayer()
+    //        setPlayer(playerDetails)
+    //    }
+    //    getPlayer()
+    //  }, [])
+
+    //fetchSinglePlayer();
+    //let {playerId} = useParams();
+      
     return (
+      
       <div>
-          <h1>Single Player</h1>
+          <h1>Single Player </h1>
+
       </div>
     );
   }
