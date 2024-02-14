1ffdf426e56f6ac93d97970da61aaab08edb9372
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-13 18:19:05 -0800

    added functionality to my player form

--- src/components/NewPlayerForm.jsx
+++ src/components/NewPlayerForm.jsx
@@ -1,83 +1,67 @@
-import { addNewPlayer } from "../API";
-import { useState, useEffect } from "react";
-//import { useParams } from 'react-router-dom'
+import { useState } from "react";
 
-function NewPlayerForm() {
-  const [name, setName] = useState(" ");
-  const [breed, setBreed] = useState(" ");
-  const [status, setStatus] = useState(" ");
-  const [imageUrl, setImageUrl] = useState(" ");
-  const [teamId, setTeamId] = useState( );
-  const [newPlayer, setNewPlayer] = useState([]);
+const NewPlayerForm = () => {
+  const [name, setName] = useState("");
+  const [breed, setBreed] = useState("");
+  const [status, setStatus] = useState( );
+  const [imageUrl, setImageUrl] = useState("");
+  const [teamId, setTeamId] = useState(Number);
+  const [error, setError] = useState(null);
+  const [players, setPlayers] = useState([]);
 
-  async function handleSubmit(event, name, breed, status, imageUrl, teamId) {
-    event.preventDefault();
-    try {
-      const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2308-acc-et-web-pt-a/players`, {
-        method: "POST" ,
-        headers: {
-          "Content-Type": "application/json",
-      },
-        body: JSON.stringify({
-          name,
-          breed,
-          status,
-          imageUrl,
-          teamId,
-      }),
-      });
-      const json = await response.json();
-        if (json.error) {
-            throw new Error(json.message);
-          }
-        init();   
-    } catch (err) {
-        console.error('Oops, something went wrong with adding that player!', err);
-    }
-    name(" ");
-    breed(" ");
-    status(" ");
-    imageUrl(" ");
-    teamId(" ");
-  }
- 
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const newPlayer = { name, breed, status, imageUrl, teamId }
+  console.log(newPlayer);
+  fetch('https://fsa-puppy-bowl.herokuapp.com/api/2308-acc-et-web-pt-a/players', {
+  method: 'POST',
+  headers: {"Content-Type": "application/json" },
+  body: JSON.stringify(newPlayer)
+}).then(() => {
+  alert('New player added');
+})
+}
+
 return (
-    <div>
+  <>
+    <div className="createPlayer">
+      
         <h1>New Player Form</h1>
-      <form onSubmit={handleSubmit}>
+        {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}  >
       <label>
         Name: 
-        <input value={name} 
-        onChange={(e) => setName(e.target.value)} />
+        <input  type="text" value={name} 
+        onChange={(e) => {setName(e.target.value)}} />
       </label>
       <label>
         Breed: 
-        <input value={breed} 
+        <input  type="text" value={breed} 
         onChange={(e) => setBreed(e.target.value)}
         />
       </label>
       <label>
         Status:
-      <select value={status} 
+      <select name="choice" value={status} 
       onChange={(e) => setStatus(e.target.value)}>
-        <option>bench</option>
-        <option>field</option>
+        <option value="bench">Bench</option>
+        <option value="field">Field</option>
       </select>
       </label>
       <label>
         Image URL:
-      <input id="imageUrl" value={imageUrl} 
+      <input  type="text" value={imageUrl} 
       onChange={(e) => setImageUrl(e.target.value)}/> 
       </label>
       <label>
         Team ID:
-      <input ivalue={teamId} type="number" 
+      <input value={teamId} type="number" 
       onChange={(e) => setTeamId(e.target.value)}/> 
       </label>
-
-      <button className="add-player">Add Player</button>
+      <button type="submit" >Add Player</button>
      </form>
     </div>
+    </>
   );
 }
 
