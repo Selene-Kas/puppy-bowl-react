f5c032e1512f6deaf5ee188a0248f67eb0d4f16c
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-13 18:12:27 -0800

    added fetch function for search bar

--- src/API.js
+++ src/API.js
@@ -1,10 +1,11 @@
 const cohortName = '2308-acc-et-web-pt-a';
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}`;
 
+
 export const fetchAllPlayers = async () => {
     try {
         const response = await fetch(`${APIURL}/players`);
-        console.log(response)
+        //console.log(response)
         const json = await response.json();
         console.log("json:", json)
         return json.data.players;
@@ -12,14 +13,32 @@ export const fetchAllPlayers = async () => {
         console.error('Uh oh, trouble fetching players!', err);
     }
 };
+//fetchAllPlayers();
 
-/**export const fetchSinglePlayer = async (playerId) => {
+//fetch for search bar
+export const fetchPlayers = (value) => {
+    fetch(`${APIURL}/players`)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      const result = json.filter((player) => {
+        return player && player.name && player.name.toLowerCase().inclues(value);
+      })
+      console.log(result);
+    });
+ };
+
+export const fetchSinglePlayer = async (playerId) => {
+
     try {
         const response = await fetch(`${APIURL}/players/${playerId}`);
         const result = await response.json();
-        console.log("player:", result.data.player);
-        return result.data.players;
+        console.log(result.data.player);
+        return result.data.player;
     } catch (err) {
         console.error(`Oh no, trouble fetching player #${playerId}!`, err);
     }
-}; **/
+};
+
+//fetchSinglePlayer(4475);
+
