92960eb2254b510aa8c23b0709c6774d38ad0e2a
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-13 18:15:58 -0800

    added search bar input

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,7 +1,9 @@
 import { fetchAllPlayers } from "../API";
-import { useState, useEffect } from "react";
+import { React, useState, useEffect } from "react";
 //import SinglePlayer from "./SinglePlayer";
 import { useNavigate } from "react-router-dom";
+
+
 
 const AllPlayers = () => {
   const [players, setPlayers] = useState([]);
@@ -20,12 +22,17 @@ const AllPlayers = () => {
   return (
     <div>
       <h1>All Players</h1>
+      <form className="searchBar" >
+        <input className="inputSearch" placeholder="Type name to search..." 
+        ></input>
+      </form>
+
       { players.map(player => {
         return (
-            <div key={player.name}>
+            <div key={player.id}>
                 <h2>{player.name}</h2>
                 <img className="puppyImg" src={player.imageUrl} />
-                <button onClick={ () => navigate(`players/${player.id}`)}> Player Details </button>
+                <button onClick={ () => navigate(`players/${player.id}`) }> Player Details </button>
             </div>
         )
       })
